710b300cd4e56365e5b41c35b98fd6fbec68b905
unknown <quang02042002@gmail.com>
2026-05-08 22:11:34 +0700

    Fix API route resolution: set global axios baseURL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,11 @@ import store from './store';
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { BASE_URL } from './constants';
 
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.withCredentials = true;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
